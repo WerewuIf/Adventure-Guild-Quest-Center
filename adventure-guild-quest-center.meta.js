961a8288be053edd8b0341fc1848367ec53af966
Werewulf <diskkitten@gmail.com>
2026-06-18 17:32:48 -0400

    Create adventure-guild-quest-center.meta.js

---
+++ adventure-guild-quest-center.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Adventure Guild Quest Center
+// @namespace    http://demonicscans.org/
+// @version      1.2
+// @description  One of the most comprehensive addons for adventure guild. AHAB only.
+// @match        https://demonicscans.org/*
+// @grant        none
+// @author       Werewulf >w<
+// @downloadURL  https://raw.githubusercontent.com/WerewuIf/Adventure-Guild-Quest-Center/main/adventure-guild-quest-center.user.js
+// @updateURL    https://raw.githubusercontent.com/WerewuIf/Adventure-Guild-Quest-Center/main/adventure-guild-quest-center.meta.js
+// ==/UserScript==
