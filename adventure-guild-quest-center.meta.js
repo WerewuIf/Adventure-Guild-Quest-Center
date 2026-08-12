6064e29588428d2a79800a635e93b8f81f25786d
Werewulf <diskkitten@gmail.com>
2026-08-12 19:28:05 -0400

    Update adventure-guild-quest-center.meta.js

--- adventure-guild-quest-center.meta.js
+++ adventure-guild-quest-center.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Adventure Guild Quest Center
 // @namespace    http://demonicscans.org/
-// @version      1.3.7
+// @version      1.3.8
 // @description  One of the most comprehensive addons for adventure guild. AHAB only.
 // @match        https://demonicscans.org/*
 // @grant        none
